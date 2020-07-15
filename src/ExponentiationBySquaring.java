c3589e4a3d618378a9a19f37e68a774af5c0d3fd
Vasilii <t9205518936@gmail.com>
2020-07-15 16:08:25 +0300

    add teachers files

---
+++ src/ExponentiationBySquaring.java
@@ -0,0 +1,7 @@
+public class ExponentiationBySquaring {
+    public static void main(String[] args) {
+        System.arraycopy();
+
+    }
+    public static int simpleExponentiationBySquaring (int item, int )
+}
---
+++ src/Lesson2/MainClass.java
@@ -0,0 +1,33 @@
+package two.online;
+
+import java.util.Arrays;
+
+public class MainClass {
+    public static void main(String[] args) {
+        //standardArrays();
+
+//        Array arr3;
+//        arr3 = new Array(5);
+
+        Array arr4 = new Array(5,2,4,3,1);
+        System.out.println(arr4);
+        arr4.sortBubble();
+        System.out.println(arr4);
+        System.out.println(arr4.hasValue(5));
+    }
+
+    private static void standardArrays() {
+        int[] arr; //int arr[]
+        arr = new int[5];
+
+        int[] arr2 = {1,2,3,4,5};
+
+        System.out.println(arr.length);
+
+        System.out.println(arr2[3]);
+
+        System.out.println(arr);
+
+        System.out.println(Arrays.toString(arr2));
+    }
+}
