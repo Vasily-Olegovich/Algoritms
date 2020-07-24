264885de9b87fd4b231a77cf4600354780953271
Vasilii <t9205518936@gmail.com>
2020-07-24 10:10:22 +0300

    first homework

--- src/ExponentiationBySquaring.java
+++
@@ -1,7 +0,0 @@
-public class ExponentiationBySquaring {
-    public static void main(String[] args) {
-        System.arraycopy();
-
-    }
-    public static int simpleExponentiationBySquaring (int item, int )
-}
